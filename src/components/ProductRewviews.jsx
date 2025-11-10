81fc32c75e530cf4c1b762cc53b327737473a809
Lokeshkumar-0806 <lokeshkumar9968142099@gmail.com>
2025-11-10 12:39:29 +0530

    fixed now

--- src/components/ProductRewviews.jsx
+++ src/components/ProductRewviews.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ProductRieviewCard from './productRieviewCard'
-import "../styles/ProductRewviews.css"
+import "../styles/productRewviews.css"
 
 const ProductRewviews = ({productReviews}) => {
   return (
